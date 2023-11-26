ed3f96fba7061c4f74fd4be30a41643a5133d61d
Brightpuritus <brightpuritus@gmail.com>
2023-11-26 21:28:13 +0700

    import
    หี

---
+++ scripsLogis&Register.js
@@ -0,0 +1,51 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.loginlink');
+const registerlink = document.querySelector('.registerlink');
+const btnPopup = document.querySelector('.btnlogin-popup');
+const close = document.querySelector('.close');
+const CPopup = document.querySelector('.Cbot-popup');
+const close2 = document.querySelector('.close2');
+const wrapper2 = document.querySelector('.wrapper2');
+
+registerlink.addEventListener('click',()=> {
+    wrapper.classList.add('active');
+});
+
+loginlink.addEventListener('click',()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=> {
+    wrapper.classList.add('active-popup');
+});
+
+close.addEventListener('click',()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+CPopup.addEventListener('click',()=> {
+    wrapper2.classList.add('activeC-popup');
+});
+
+close2.addEventListener('click',()=> {
+    wrapper2.classList.remove('activeC-popup');
+});
+
+let e = document.getElementById("mal");
+let p = document.getElementById("pass");
+let re = document.getElementById("result");
+
+function CheckPassword(){
+    let em = e.value;
+    let Password = p.value;
+
+    if(em === "brightpuritus" || Password === "260248"){
+        
+        // re.innerHTML = "Login success";
+        let url = p2.html
+        let newwindow = window.open(url);
+    }
+    else{
+        re.innerHTML = "Login Failed";
+    }
+}
